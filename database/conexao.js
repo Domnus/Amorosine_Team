d024836462d117048ba25dfd9c85f490cf0a67cc
Cremonez <fernandocremonez@univem.edu.br>
2021-09-28 14:03:50 -0300

    Formulário adicionado e host alterado

--- database/conexao.js
+++ database/conexao.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const conexao = mysql.createConnection({
-	host: '192.168.1.12',
+	host: 'localhost',
 	port: 3306,
 	user: 'ong_amorosine',
 	password: 'amorosine',
