4566beb9c559b038133c28a25c8115e57c063429
Domnus <bcsilva49@gmail.com>
2021-11-10 16:58:41 -0300

    Começo de modals

--- front-end/admPage.js
+++ front-end/admPage.js
@@ -1,32 +1,55 @@
-const acoes_url = "http://localhost:3000/acoes"
+window.onload = function() {
+	/* Pegar dados do banco */
+	const acoes_url = "http://localhost:3000/acoes"
+	const eventosurl = "http://localost:3000/eventos"
 
-async function getData(url) {
-	const response = await fetch(url);
+	async function getData(url) {
+		const response = await fetch(url);
 
-	var data = await response.json();
-	console.log(data);
-	show(data);
-}
-
-function show(data) {
-	let tab = 
-		`
-		<tr>
-			<th>Nome</th>
-			<th>Descrição</th>
-		</t>
-		`
-
-	for (i = 0; i < data.length; i++) {
-		tab += `
-				<tr>
-					<td>${data[i].nome}</td>
-					<td>${data[i].descricao}</td>
-				</tr>
-				`
+		var data = await response.json();
+		console.log(data);
+		show(data);
 	}
 
-	document.getElementById("acoesSociais").innerHTML = tab;
-}
+	function show(data) {
+		let tab = 
+			`
+			<tr>
+				<th>Nome</th>
+				<th>Descrição</th>
+			</t>
+			`
 
-getData(acoes_url);
+		for (i = 0; i < data.length; i++) {
+			tab += `
+					<tr>
+						<td>${data[i].nome}</td>
+						<td>${data[i].descricao}</td>
+					</tr>
+					`
+		}
+
+		document.getElementById("acoesSociais").innerHTML = tab;
+	}
+
+	getData(acoes_url);
+
+	/* Modal */
+	var addAcaoModal = document.getElementById("addAcaoModal");
+
+	var addButton = document.getElementById("addAcao");
+
+	var span = document.getElementById("close");
+
+	addButton.onclick = function() {
+		addAcaoModal.style.display = "block";
+	}
+
+
+
+	window.onclick = function (event) {
+		if (event.target == addAcaoModal) {
+			addAcaoModal.style.display = "none";
+		}
+	}
+}
